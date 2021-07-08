3d8f104cf068602103e92f06acb0d22a5d274bd1
Zheng Xi Zhou <zzxwill@gmail.com>
2021-07-08 13:41:44 +0800

    Support Alibaba Cloud StsToken (#50)
    Implemented the support of Environment ALICLOUD_SECURITY_TOKEN to
    provisio cloud resources in Alibaba Cloud.

--- hack/prepare-alibaba-credentials.sh
+++ hack/prepare-alibaba-credentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "accessKeyID: ${ALICLOUD_ACCESS_KEY}\naccessKeySecret: ${ALICLOUD_SECRET_KEY}" > alibaba-credentials.conf
+echo "accessKeyID: ${ALICLOUD_ACCESS_KEY}\naccessKeySecret: ${ALICLOUD_SECRET_KEY}\nsecurityToken: ${ALICLOUD_SECURITY_TOKEN}" > alibaba-credentials.conf
 kubectl create secret generic alibaba-account-creds -n vela-system --from-file=credentials=alibaba-credentials.conf
 rm -f alibaba-credentials.conf
