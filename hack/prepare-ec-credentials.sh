44eeeb20b17d550988cbe42a6d1150eb76ffbe11
kirby@puppetlabs.com <mk.kirby@gmail.com>
2021-08-17 11:34:25 -0700

    Update example EC to make it usable
    This change updates the example EC configuration so it is possible to
    use. Without this change typos and bugs prevented it from being usable.

--- hack/prepare-ec-credentials.sh
+++ hack/prepare-ec-credentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "ecApiKey: '${EC_API_KEY}'" > ec-credentials.conf
+echo "ecApiKey: ${EC_API_KEY}" > ec-credentials.conf
 kubectl create secret generic ec-account-creds -n vela-system --from-file=credentials=ec-credentials.conf
 rm -f ec-credentials.conf
