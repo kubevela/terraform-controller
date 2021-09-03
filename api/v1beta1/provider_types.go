37c2a1f67ee43ecae1670056adac3bc1f124e798
zzxwill <zzxwill@gmail.com>
2021-09-03 14:43:21 +0800

    add colunmn `age`

--- api/v1beta1/provider_types.go
+++ api/v1beta1/provider_types.go
@@ -55,6 +55,7 @@ type ProviderStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="STATE",type="string",JSONPath=".status.state"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 
 // Provider is the Schema for the providers API.
 type Provider struct {
