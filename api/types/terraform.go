260920a898871cf14b93d7c0ac9e55ad8262238d
zzxwill <zzxwill@gmail.com>
2021-09-06 14:10:27 +0800

    Add hcl/json syntax validation ability before executing terraform
    Check the syntax for hcl/json before creating a job to running
    terraform init && apply

--- api/types/terraform.go
+++ api/types/terraform.go
@@ -6,3 +6,13 @@ const (
 	// TerraformHCLConfigurationName is the file name for Terraform hcl Configuration
 	TerraformHCLConfigurationName = "main.tf"
 )
+
+// ConfigurationType is the type for Terraform Configuration
+type ConfigurationType string
+
+const (
+	// ConfigurationJSON is the json type Configuration
+	ConfigurationJSON ConfigurationType = "JSON"
+	// ConfigurationHCL is the HCL type Configuration
+	ConfigurationHCL ConfigurationType = "HCL"
+)
---
+++ controllers/configuration/configuration_test.go
@@ -0,0 +1,141 @@
+package configuration
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	v1 "k8s.io/api/core/v1"
+)
+
+func TestCompareTwoContainerEnvs(t *testing.T) {
+	cases := map[string]struct {
+		s1   []v1.EnvVar
+		s2   []v1.EnvVar
+		want bool
+	}{
+		"Equal": {
+			s1: []v1.EnvVar{
+				{
+					Name:  "TF_VAR_zone_id",
+					Value: "cn-beijing-z",
+				},
+				{
+					Name:  "E1",
+					Value: "V1",
+				},
+				{
+					Name:  "NAME",
+					Value: "aaa",
+				},
+			},
+			s2: []v1.EnvVar{
+
+				{
+					Name:  "NAME",
+					Value: "aaa",
+				},
+				{
+					Name:  "TF_VAR_zone_id",
+					Value: "cn-beijing-z",
+				},
+				{
+					Name:  "E1",
+					Value: "V1",
+				},
+			},
+			want: true,
+		},
+		"Not Equal": {
+			s1: []v1.EnvVar{
+				{
+					Name:  "NAME",
+					Value: "aaa",
+				},
+			},
+			s2: []v1.EnvVar{
+
+				{
+					Name:  "NAME",
+					Value: "bbb",
+				},
+			},
+			want: false,
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			equal := CompareTwoContainerEnvs(tc.s1, tc.s2)
+			if equal != tc.want {
+				if diff := cmp.Diff(tc.want, equal); diff != "" {
+					t.Errorf("\nCompareTwoContainerEnvs(...) %s\n", diff)
+				}
+			}
+		})
+	}
+}
+
+func TestCheckTFConfiguration(t *testing.T) {
+	cases := map[string]struct {
+		configuration string
+		subStr        string
+	}{
+		"Invalid": {
+			configuration: `resource2 "alicloud_oss_bucket" "bucket-acl" {
+  bucket = var.bucket
+  acl = var.acl
+}
+
+output "BUCKET_NAME" {
+  value = "${alicloud_oss_bucket.bucket-acl.bucket}.${alicloud_oss_bucket.bucket-acl.extranet_endpoint}"
+}
+
+variable "bucket" {
+  description = "OSS bucket name"
+  default = "vela-website"
+  type = string
+}
+
+variable "acl" {
+  description = "OSS bucket ACL, supported 'private', 'public-read', 'public-read-write'"
+  default = "private"
+  type = string
+}
+`,
+			subStr: "Error:",
+		},
+		"valid": {
+			configuration: `resource "alicloud_oss_bucket" "bucket-acl" {
+  bucket = var.bucket
+  acl = var.acl
+}
+
+output "BUCKET_NAME" {
+  value = "${alicloud_oss_bucket.bucket-acl.bucket}.${alicloud_oss_bucket.bucket-acl.extranet_endpoint}"
+}
+
+variable "bucket" {
+  description = "OSS bucket name"
+  default = "vela-website"
+  type = string
+}
+
+variable "acl" {
+  description = "OSS bucket ACL, supported 'private', 'public-read', 'public-read-write'"
+  default = "private"
+  type = string
+}`,
+			subStr: "",
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			err := checkTerraformSyntax(tc.configuration)
+			if err != nil {
+				if !strings.Contains(err.Error(), tc.subStr) {
+					t.Errorf("\ncheckTFConfiguration(...) %s\n", cmp.Diff(err.Error(), tc.subStr))
+				}
+			}
+		})
+	}
+}
