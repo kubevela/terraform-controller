0d4c35b217b513e98f26c26688c9348728ea3f75
zzxwill <zzxwill@gmail.com>
2021-10-26 17:38:25 +0800

    debug: describe pod

--- e2e/cloud-providers/alibaba_test.go
+++ e2e/cloud-providers/alibaba_test.go
@@ -36,6 +36,11 @@ func TestConfiguration(t *testing.T) {
 			Expect(err).To(BeNil())
 			fmt.Println("Checking Terraform Controller status")
 			fmt.Println(string(tfPod))
+
+			tfPodDetails, err := exec.Command("bash", "-c", "kubectl describe pod -n terraform").Output()
+			Expect(err).To(BeNil())
+			fmt.Println("Describing Terraform Controller Pod")
+			fmt.Println(string(tfPodDetails))
 			
 			output, err := exec.Command("bash", "-c", "kubectl get configuration").Output()
 			Expect(err).To(BeNil())
